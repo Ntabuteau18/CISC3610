18a6663c40785780bb28ee0d551ff027662bcb76
Ntabuteau18 <Ntabuteau60@gmail.com>
2022-09-26 15:30:04 -0400

    updates

---
+++ chart.js
@@ -0,0 +1,7 @@
+var fruit = [
+    { name: "Strawberry", quantity: 15, color: "red" },
+    { name: "Peach", quantity: 10, color: "orange" },
+    { name: "Banana", quantity: 25, color: "yellow" },
+    { name: "Pear", quantity: 10, color: "green" },
+    { name: "Grape", quantity: 20, color: "purple" }
+];

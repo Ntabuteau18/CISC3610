c99adacf6ac7a6282fc209cb3518a009523df72b
Ntabuteau18 <Ntabuteau60@gmail.com>
2022-09-29 12:45:09 -0400

    updates

--- chart.js
+++ chart.js
@@ -5,3 +5,26 @@ var fruit = [
     { name: "Pear", quantity: 10, color: "green" },
     { name: "Grape", quantity: 20, color: "purple" }
 ];
+
+var canvas = document.getElementById("mycanvas");
+var ctxx = canvas.getContext("2d");
+
+var chart =function (){
+    
+    var x=0;
+    for (var i=0;i<fruit.length;i++){
+
+    ctxx.fillStyle = fruit[i].color;
+    ctxx.fillRect(x,canvas.height-20* fruit[i].quantity,95,600);
+
+    ctxx.fillStyle = "black";
+    ctxx.font = "bold 20px Calibri";
+    
+    ctxx.fillText(fruit[i].name,x,650);
+    ctxx.fillText(fruit[i].quantity,x,630);
+    ctxx.stroke();
+    x=x+120;
+    }
+};
+
+chart();
